898140cb5d3d673ba7c51d29486a3301aac54d8e
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-02-26 17:36:21 -0500

    half way done with website

--- style.js
+++ style.js
@@ -1,25 +1,24 @@
 function currentSlide(n) {
     var testimonials = document.getElementsByClassName("testimonial");
     var indicators = document.getElementsByClassName("testimonial-indicators")[0].getElementsByTagName("li");
-  
+
     for (var i = 0; i < testimonials.length; i++) {
-      testimonials[i].style.display = "none";
-      indicators[i].className = indicators[i].className.replace(" active", "");
-    } 
-  
-    testimonials[n - 1].style.display = "block";
-    indicators[n - 1].className += " active";
-  }
-  
-  // Add event listeners to the indicators
-  document.addEventListener("DOMContentLoaded", function() {
+        testimonials[i].classList.remove("active");
+        indicators[i].classList.remove("active");
+    }
+
+    testimonials[n - 1].classList.add("active");
+    indicators[n - 1].classList.add("active");
+}
+
+// Attach event listeners to the indicators
+window.onload = function() {
     var indicators = document.getElementsByClassName("testimonial-indicators")[0].getElementsByTagName("li");
     for (var i = 0; i < indicators.length; i++) {
-      (function(index) {
-        indicators[index].addEventListener("click", function() {
-          currentSlide(index + 1);
-        });
-      })(i);
+        (function(index) {
+            indicators[index].addEventListener("click", function() {
+                currentSlide(index + 1);
+            });
+        })(i);
     }
-  });
-  
+};
